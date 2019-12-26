47c3cf2962c7a2b1c351aefb944ad09f2f07f9e3
Johannes <office@atelierbuchner.at>
2019-12-26 16:31:10 +0100

    new stuff

---
+++ 7kyu Isograms.js
@@ -0,0 +1,14 @@
+function isIsogram(str) {
+  let arr = str
+    .toLowerCase()
+    .split("")
+    .sort();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+isIsogram("aba");
