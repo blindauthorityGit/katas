7ca3dc158ded36d6442ae0ce3de11bce3883119a
Johannes <office@atelierbuchner.at>
2019-12-26 16:01:01 +0100

    new stuff

---
+++ 5kyu RGB To Hex Conversion.js
@@ -0,0 +1,26 @@
+function rgb(r, g, b) {
+  let arr = [r, g, b];
+  let res = [];
+
+  arr.forEach(function(x) {
+    if (x > 255) {
+      x = 255;
+    } else if (x <= 0) {
+      x = "00";
+    }
+    res.push(x.toString(16));
+  });
+
+  for (let i = 0; i < res.length; i++) {
+    if (res[i].length === 1) {
+      res[i] = "0" + res[i];
+    }
+  }
+
+  return res
+    .join("")
+    .toString()
+    .toUpperCase();
+}
+
+rgb(249, 166, 10);
