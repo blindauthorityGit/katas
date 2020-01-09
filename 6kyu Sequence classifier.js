a06207ab9b4d0f00996ebb73fa7b85d9f78172d5
Johannes <office@atelierbuchner.at>
2020-01-09 15:03:32 +0100

    new Stuff

--- 6kyu Sequence classifier.js
+++ 6kyu Sequence classifier.js
@@ -1,11 +1,23 @@
 function sequenceClassifier(arr) {
   if (arr.every(e => e === arr[0])) {
-    console.log(5);
-  }
-  if (arr.every((number, i) => i === 0 || arr[i - 1] < number)) {
-    console.log(1);
+    return 5;
+  } else if (arr.every((number, i) => i === 0 || arr[i - 1] < number)) {
+    return 1;
+  } else if (
+    arr.every(
+      (number, i) => i === 0 || arr[i - 1] < number || arr[i - 1] === number
+    )
+  ) {
+    return 2;
+  } else if (arr.every((number, i) => i === 0 || arr[i - 1] > number)) {
+    return 3;
+  } else if (
+    arr.every(
+      (number, i) => i === 0 || arr[i - 1] > number || arr[i - 1] === number
+    )
+  ) {
+    return 4;
+  } else {
+    return 0;
   }
 }
-//your code here
-
-sequenceClassifier([3, 5, 8, 9, 14, 23]);
