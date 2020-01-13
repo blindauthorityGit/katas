f60b60ff50c819d63135cd06532bfdc11dd9cd37
Johannes <office@atelierbuchner.at>
2020-01-13 02:15:56 +0100

    new stuff

--- 6kyu Sides of an Array.js
+++ 6kyu Sides of an Array.js
@@ -1,18 +1,46 @@
 function findEvenIndex(arr) {
-  let sumL = [];
-  let sumR = [];
+  let sumL = 0;
   let arrCopy = [...arr];
   let right = [];
+  let left = [];
+  let res = [];
 
-  console.log(arr);
-  for (let i = 0; i < arr.length - 1; i++) {
-    arrCopy.shift();
-    if (arr[i] !== undefined) {
-      console.log(arrCopy.reduce((a, c) => a + c));
-      right.push(arrCopy.reduce((a, c) => a + c));
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j - 1] === undefined) {
+      left.push(0);
+    } else {
+      sumL += arr[j - 1];
+      left.push(sumL);
     }
   }
-  console.log(right);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 1] === undefined) {
+      right.push(0);
+      break;
+    }
+    arrCopy.shift();
+    if (arr[i] !== undefined && arr[i] !== null) {
+      right.push(arrCopy.reduce((a, c) => a + c));
+    } else {
+      right.push(0);
+    }
+  }
+
+  for (let k = 0; k < left.length; k++) {
+    for (let l = 0; l < right.length; l++) {
+      if (left[k] === right[k]) {
+        res.push(k);
+        break;
+      }
+    }
+  }
+
+  if (res.length != 0) {
+    return parseInt(res, 10);
+  } else {
+    return -1;
+  }
 }
 
 findEvenIndex([1, 2, 3, 4, 3, 2, 1]);
